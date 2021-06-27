8f4a47ae84bbc9eb878f807d488639d70e552d57
Andres Largo <felipeflogxd@gmail.com>
2021-06-27 02:27:14 -0500

    Fetch api test suite

---
+++ src/__test__/utils/getData.test.js
@@ -0,0 +1,17 @@
+import { beforeEach, expect, test } from '@jest/globals';
+import getData from '../../utils/getData';
+
+describe('Fetch api', () => {
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
+
+  test('Llamar api', () => {
+    fetch.mockResponseOnce(JSON.stringify({ data: '12345' }));
+    getData('https://google.com').then(response => {
+      expect(response.data).toEqual('12345');
+    });
+
+    expect(fetch.mock.calls[0][0]).toEqual('https://google.com');
+  });
+});
