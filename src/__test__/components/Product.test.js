58d8601e369c0303a4e8b664fc21f7cd260a90d2
Andres Largo <felipeflogxd@gmail.com>
2021-06-27 01:45:36 -0500

    Product test

--- src/__test__/components/Product.test.js
+++ src/__test__/components/Product.test.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { expect, test } from '@jest/globals';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Product from '../../components/Product';
 import ProviderMock from '../../__mocks__/ProviderMock';
+import ProductMock from '../../__mocks__/ProductMock';
 
 describe('<Product />', () => {
-  test('Render componente Product', () => {});
-
-  test('Render título', () => {
-    const header = mount(
+  test('Render componente Product', () => {
+    const product = shallow(
       <ProviderMock>
         <Product />
       </ProviderMock>
     );
-    expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+    expect(product.length).toEqual(1);
+  });
+
+  test('Comprobar el botón de comprar', () => {
+    const handle = jest.fn();
+    const wrapper = mount(
+      <ProviderMock>
+        <Product product={ProductMock} handleAddToCart={handle} />
+      </ProviderMock>
+    );
+    wrapper.find('button').simulate('click');
+    expect(handle).toHaveBeenCalledTimes(1);
   });
 });
