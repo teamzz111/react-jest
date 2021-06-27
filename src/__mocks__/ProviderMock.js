45c312e39b2cce686449faeeb93291967d02fafa
Andres Largo <felipeflogxd@gmail.com>
2021-06-27 01:45:27 -0500

    Fix imports

--- src/__mocks__/ProviderMock.js
+++ src/__mocks__/ProviderMock.js
@@ -4,8 +4,8 @@ import { createStore } from 'redux';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createBrowserHistory } from 'history';
-import initialState from '../../initialState';
-import reducer from '../../reducers/index';
+import initialState from '../initialState';
+import reducer from '../reducers/index';
 
 const store = createStore(reducer, initialState);
 const history = createBrowserHistory();
