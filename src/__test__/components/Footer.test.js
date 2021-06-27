dcff0664d0d839c0c6a44d2dc918f7822ffbfad4
Andres Largo <felipeflogxd@gmail.com>
2021-06-27 01:49:31 -0500

    Snapshot

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,11 +1,20 @@
 import 'jsdom-global/register';
 import React from 'react';
 import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
+import { expect, test } from '@jest/globals';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
   test('Render del componente Footer', () => {
     const footer = mount(<Footer />);
     expect(footer.length).toEqual(1);
+  });
+});
+
+describe('Footer Snapshot', () => {
+  test('Comprobar UI únicamente', () => {
+    const footer = create(<Footer />);
+    expect(footer.toJSON()).toMatchSnapshot();
   });
 });
